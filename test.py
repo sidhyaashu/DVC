3658d49d6e6005a978d7224ef04d38116973f6b2
sidhyaashu <ashutoshsidhya69@gmail.com>
2024-10-06 13:21:56 +0530

    done

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 import pandas as pd
+
+Data =[
+    {"name":"Asutosh sidhya","age":20,"city":"Kolkata"},
+    {"name":"Rama sidhya","age":21,"city":"local"},
+    {"name":"Signal sidhya","age":22,"city":"pak"},
+]
+
+
+
+
+
+Data = pd.DataFrame(Data)
+Data.to_csv("data/data.csv",index=False)
